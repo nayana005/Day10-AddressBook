351a1846ee9b649e49b90037af960d73f585b2a9
nayana005 <nayanasuresh1005@gmail.com>
2022-08-19 00:07:44 +0530

    Added: Edit Contact

--- Main.java
+++ Main.java
@@ -1,13 +1,46 @@
 package com.bridgelabz.address_book10;
 
+import java.util.Scanner;
+
 public class Main {
-	
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         AddresBook addressBook = new AddresBook();
         System.out.println("Welcome to Address Book Program ");
-        addressBook.addNewContacts();
-        addressBook.showContacts();
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+
+        // While loop for showing the menu until user not want to exit
+        while(!exit) {
+        System.out.println("\nEnter 1. Add new Contact \nEnter 2. Edit Contact " +
+                "\nEnter 3. Show Contacts \nEnter 4. Exit");
+        int num = sc.nextInt();
+
+        // writing the switch case
+            switch (num) {
+
+                case 1:
+                    addressBook.addNewContacts();
+                    break;
+
+                case 2:
+                    addressBook.editContact();
+                    break;
+                case 3:
+                    addressBook.showContacts();
+                    break;
+
+                case 4:
+                    System.out.println("Exit !");
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("Enter the correct number !");
+                    break;
+            }
+        }
     }
 }
 
