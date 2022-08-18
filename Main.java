a8e736899ccdecde03a76175154b11027819ea3a
nayana005 <nayanasuresh1005@gmail.com>
2022-08-18 23:52:52 +0530

    Added: Create Contact

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.address_book10;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program ");
+        // creating a contact
+        Contact contacts1 = new Contact("Nayana","Suresh","001 thimmarayappa garden",
+                "nayanasuresh1005@gmail.com","Bangalore","Karnataka",560005,123456789);
+        System.out.println(contacts1);
+    }
+}
